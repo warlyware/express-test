26db3f080fd42ebc7b4249231895fff503150559
warlyware <danward@gmail.com>
2015-06-29 13:12:10 -0700

    Add basic jade translation of personal page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', color: 'red' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test', {});
+});
+
+
+router.get('/aboutus', function(req, res, next) {
+  res.render('aboutus', {});
 });
 
 module.exports = router;
